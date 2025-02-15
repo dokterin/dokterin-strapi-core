780d61802bdc2a2c9862029781e9bc771e04914d
Maulana Adam Sahid <masadamsahid@gmail.com>
2025-02-15 11:17:57 +0700

    integrate with cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,7 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +9,24 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
